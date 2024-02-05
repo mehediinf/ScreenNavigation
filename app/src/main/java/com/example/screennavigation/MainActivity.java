3df65f13d3d8f71b163eb299cf9a7c5ae0d688f3
Md Mehedi Hasan <145797601+mehediinf@users.noreply.github.com>
2024-02-06 02:11:07 +0600

    Second Commit ScreenNavigation

--- app/src/main/java/com/example/screennavigation/MainActivity.java
+++ app/src/main/java/com/example/screennavigation/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.screennavigation;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout_Id);
+        viewPager = findViewById(R.id.viewPager_Id);
+
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                viewPager.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
+
+    class MyPagerAdapter extends FragmentPagerAdapter{
+
+        String[] text = {"Australia","Bangladesh","India"};
+
+
+        public MyPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+
+            if (position==0){
+                return new AustraliaFragment();
+            }else if (position==1){
+                return new BangladeshFragment();
+            }else if (position==2){
+                return new IndiaFragment();
+            }
+
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return text.length;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return text[position];
+        }
+    }
+
 }
